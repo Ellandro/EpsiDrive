ad859c18fa53a656d6152645a6a5ef0122e2c266
Adama Kouadio Landry <118170546+Ellandro@users.noreply.github.com>
2023-11-24 16:50:33 +0000

    Fin de footer et de header

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
+@app.route("/sidebar/")
+def sidebar():
+    return render_template('./partials/sidebar.html')
 
+@app.route("/menu/")
+def menu():
+    return render_template('./dashbord/base.html')
 if __name__ == "__main__":
     app.run(debug=True)
