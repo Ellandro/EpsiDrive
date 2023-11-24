c13eb9923ada4ca28f7cc3f7c1c5e03b9031e931
aamenan <cyrilleallangba69@gmail.com>
2023-11-24 17:53:38 +0000

    Premier commit dashboard utilisateur

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def hello_world():
     return render_template("index.html")
 
 
+@app.route("/interface_users")
+def interface_users():
+    return render_template("./users/interface_users.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
