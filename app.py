58927117ad0a40699351f5128b2f8dd4b690fb18
Adama Kouadio Landry <118170546+Ellandro@users.noreply.github.com>
2023-11-26 23:24:15 +0000

    Finalisation du dashboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash, url_for
 
 
 app = Flask(__name__)
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-@app.route("/sidebar/")
+@app.route("/menu/")
 def sidebar():
-    return render_template('./partials/sidebar.html')
+    return render_template('./dashbord/base.html')
+
+@app.route('/user/')
+def user():
+    return render_template('./users/interface_users.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
